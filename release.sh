21faaadaf64bbb80ff823a8a6d306b5434bfabb1
Aleksandr Razumov <ar@gortc.io>
2018-09-25 00:28:37 +0300

    separate archives for mars mission and tutorial

--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-zip -r Language.zip *.xml -x english*
+zip -r Language.zip *.xml -x english* -x *_mars_mission.xml -x *_tutorial.xml
+zip -r LanguageMarsMission.zip *_mars_mission.xml -x english*
+zip -r LanguageTutorial.zip *_tutorial.xml -x english*
