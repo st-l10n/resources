d01921d7e1a28c16cb0852ad61d1d1c999c44341
Aleksandr Razumov <ar@gortc.io>
2018-09-25 03:56:44 +0300

    use game structure of files instead plain one

--- release.sh
+++ release.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-zip -r Language.zip *.xml -x english* -x *_mars_mission.xml -x *_tutorial.xml
-zip -r LanguageMarsMission.zip *_mars_mission.xml -x english*
-zip -r LanguageTutorial.zip *_tutorial.xml -x english*
+zip -r StreamingAssets.zip -@ < assets.txt
